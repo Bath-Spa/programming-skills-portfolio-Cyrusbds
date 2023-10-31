7a31a6d1ab145dd11415b7886eff31c8bcbe43f5
Cyrus <144656478+Cyrusbds@users.noreply.github.com>
2023-10-31 22:08:58 +0400

    update
    main

--- Chapter 2- Variables & Comments/Exercises/chapter2exercises.py
+++ Chapter 2- Variables & Comments/Exercises/chapter2exercises.py
@@ -1,24 +1,24 @@
-#Exercise 1
+#Exercise 1: Variables
 M = ("I want to Sleep")
 print(M)
 M = ("One More Episode")
 print(M)
 
-#Exercise 2
+#Exercise 2: Variables
 print("Master Shifu once said, “If you only do what you can do\nyou can never be more than you are.")
 
-#Exercise 3
+#Exercise 3: Stripping Names
 a = "\tCyrus DS\n"
 print(a)
 print(a.lstrip())
 print(a.rstrip())
 print(a.strip())
 
-#Exercise 4
+#Exercise 4: Favorite Number
 favnum = int(input("What is your favorite number? "))
 print("Your Favorite number is", favnum)
 
-#Exercise 5
+#Exercise 5: USB Shopper
 usb_girl = ("USB Sticks she can buy = ")
 pounds_left = ("Pounds she will have left = ")
 print(usb_girl + str(int(50/6)))
